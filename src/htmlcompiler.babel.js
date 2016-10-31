bdb42643f5d7e5359f600a1fc0396608bdbe7dfe
nojaja <free.riccia@gmail.com>
2016-11-01 00:57:27 +0900

    Update htmlcompiler.babel.js

--- src/htmlcompiler.babel.js
+++ src/htmlcompiler.babel.js
@@ -12,10 +12,13 @@ Copyright 2016 - 2016
     getNodes(arg) {
       return this.nodes.join(arg || '\n');
     }
+    addNode(node) {
+      this.nodes.push(node);
+    }
 
     mkAttribute_text(compiler, key, attribute, node) {}
     mkAttribute_script(compiler, key, attribute, node) {}
-    mkTagElement_open(compiler, src, isContainer) {}
+    mkTagElement_open(compiler, src, attributes, isContainer) {}
     mkTagElement_close(compiler, src) {}
     mkTextElement(compiler, src) {}
     mkScriptElement_open(compiler, src, isContainer) {}
@@ -24,16 +27,16 @@ Copyright 2016 - 2016
 
   class HtmlBuilder extends Builder{
       mkAttribute_text(compiler, key, attribute, node) {
-        node.push(`${key}='${attribute.data||''}'`);
+        return(`${key}='${attribute.data||''}'`);
       }
-      mkTagElement_open(compiler, src, isContainer) {
-        this.nodes.push(`<${src.name}${src.attributes?' '+compiler.mkAttribute(src.attributes,this):''} ${isContainer?'':'/'}>`);
+      mkTagElement_open(compiler, src, attributes, isContainer) {
+        return(`<${src.name}${attributes} ${isContainer?'':'/'}>`);
       }
       mkTagElement_close(compiler, src) {
-        this.nodes.push(`</${src.name}>`);
+        return(`</${src.name}>`);
       }
       mkTextElement(compiler, src) {
-        this.nodes.push(`${src.data}`);
+        return(`${src.data}`);
       }
   }
 
@@ -50,11 +53,11 @@ Copyright 2016 - 2016
         attributes[attrkey].forEach(function(attribute) {
           if(attribute.type == 'script') {
             //this._builders.forEach(function(_builder) {
-              _builder.mkAttribute_script(this, attrkey, attribute, node);
+              node.push(_builder.mkAttribute_script(this, attrkey, attribute, node));
             //}, this);
           } else {
             //this._builders.forEach(function(_builder) {
-              _builder.mkAttribute_text(this, attrkey, attribute, node);
+              node.push(_builder.mkAttribute_text(this, attrkey, attribute, node));
             //}, this);
           }
         }, this);
@@ -64,36 +67,38 @@ Copyright 2016 - 2016
     mkTagElement(src) {
       if (src.children) {
         this._builders.forEach(function(_builder) {
-          _builder.mkTagElement_open(this, src, true);
+          var attributes = src.attributes?' '+this.mkAttribute(src.attributes,_builder):'';
+          _builder.addNode(_builder.mkTagElement_open(this, src, attributes, true));
         }, this);
         src.children.forEach(function(_src) {
           this.mkNodes(_src);
         }, this);
 
         this._builders.forEach(function(_builder) {
-          _builder.mkTagElement_close(this, src);
+          _builder.addNode(_builder.mkTagElement_close(this, src));
         }, this);
 
       } else {
         this._builders.forEach(function(_builder) {
-          _builder.mkTagElement_open(this, src, false);
+          var attributes = src.attributes?' '+this.mkAttribute(src.attributes,_builder):'';
+          _builder.addNode(_builder.mkTagElement_open(this, src, attributes, false));
         }, this);
       }
     }
     mkScriptElement(src) {
       if (src.children) {
         this._builders.forEach(function(_builder) {
-          _builder.mkScriptElement_open(this, src, true);
+          _builder.addNode(_builder.mkScriptElement_open(this, src, true));
         }, this);
         src.children.forEach(function(_src) {
           this.mkNodes(_src);
         }, this);
         this._builders.forEach(function(_builder) {
-          _builder.mkScriptElement_close(this, src);
+          _builder.addNode(_builder.mkScriptElement_close(this, src));
         }, this);
       } else {
         this._builders.forEach(function(_builder) {
-          _builder.mkScriptElement_open(this, src, false);
+          _builder.addNode(_builder.mkScriptElement_open(this, src, false));
         }, this);
       }
     }
@@ -101,9 +106,9 @@ Copyright 2016 - 2016
       if (src.type == 'tag') this.mkTagElement(src);
       if (src.type == 'text')
         this._builders.forEach(function(_builder) {
-          _builder.mkTextElement(this,src);
+          _builder.addNode(_builder.mkTextElement(this,src));
         }, this);
-      if (src.type == 'script') this.mkScriptElement(src);
+      if (src.type == 'script')this.mkScriptElement(src);
       return;
     }
     compile(src) {
