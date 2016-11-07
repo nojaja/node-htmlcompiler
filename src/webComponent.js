891e8a5197903bb3bb188e0026538f3988fb33ee
nojaja <free.riccia@gmail.com>
2016-11-07 22:43:19 +0900

    Update webComponent.js
    mod yield and clean

--- src/webComponent.js
+++ src/webComponent.js
@@ -23,8 +23,6 @@ webComponentBuilder.beforeCompile = function (src){
 
 webComponentBuilder.expansionAttributes = function (attributes,parameters) {
   var _attributes = {};
-//console.log('parameters',parameters);
-//console.log('attributes',attributes);
   for (var key in attributes) {
     var attribute = attributes[key];
     var _attribute = attributes[key];
@@ -55,77 +53,50 @@ webComponentBuilder.cloneElement = function (customElement,src, parentNode,isCon
   if(customElement.langName)_customElement.langName = customElement.langName;
   if(customElement.data)_customElement.data = customElement.data;
 
+  //srcの子要素に入れ替えるタグ
+  if(customElement.name=='yield'){
+    if(src.children){
+      for (var j = 0; j < src.children.length; j ++) {
+        parentNode.children.push(src.children[j]);
+      }
+    }
+    return;
+  }
+  
+  //スクリプトの展開
   if(customElement.type=='script' && customElement.langName=='singleMustache'){
         var key = customElement.data.trim();
         var __customElement = src.attributes[key];
-          console.log('script',customElement,src.attributes,key,__customElement);
         if(__customElement){
           _customElement.data = __customElement[0].data;
           _customElement.type = __customElement[0].type;
           delete _customElement.langName;
-          console.log(_customElement);
-          /*
-          //属性を入れ替える
-          _customElement.children = src.attributes.slice(0, i-1)
-            .concat(src.attributes[customElement.data])
-            .concat(src.attributes.slice(i+1, src.attributes.length));
         }else{
-          _customElement.children = src.attributes.slice(0, i-1)
-            .concat(src.attributes.slice(i+1, src.attributes.length));
-        }
-        */
+          return;//対象の変数が無ければスキップ
         }
    }
   
-  if(customElement.name=='yield'){
-    //if(isContainer && src.children)_customElement.children = src.children;
-  }
-  
   if(customElement.attributes)_customElement.attributes = webComponentBuilder.expansionAttributes(customElement.attributes,src.attributes);
-  
   if(customElement.children){  
     _customElement.children = [];
-    
     for (var i = 0; i < customElement.children.length; i ++) {
       var child = customElement.children[i];
-      _customElement.children.push(webComponentBuilder.cloneElement(child,src, _customElement,isContainer));
+      webComponentBuilder.cloneElement(child,src, _customElement,isContainer);
     }
   };
+  parentNode.children.push(_customElement);
   return _customElement;
 }
+
 //webComponentを展開
 webComponentBuilder.beforeCreateTagElement = function (src, isContainer) {
   if(webComponentBuilder.elements[src.name]){
-    //var customElement = Object.assign({},webComponentBuilder.elements[src.name]);
-    
-    var _customElement = webComponentBuilder.elements[src.name];
-    
-    //var customElement = clone(_customElement,null, null, [_customElement.parentNode]);
-    //var template = customElement.template[0];
+    var _customElement = webComponentBuilder.elements[src.name]; 
     var _template = _customElement.template[0];
     var template = webComponentBuilder.cloneElement(_template,src,src.parentNode,isContainer);
-    //src.name = template.name;
-    //src.type = template.type;
-    //src.attributes = webComponentBuilder.expansionAttributes(template.attributes,src.attributes);
-    
-    //TODO cloneをやめて再帰的にexpansionAttributesをやらないとだめ
-    /*
-    var children = [];
-    if(template.children)template.children.forEach(function(child){
-      if(child.name=='yield'){
-        if(isContainer && src.children)children.push(clone(src.children,null, null, [src.children.parentNode]));
-      }else{
-        children.push(clone(child,null, null, [child.parentNode]));
-      }
-    });
-    src.children = children;
-    */
-      console.log('template',template);
-    //src=template;
-      src.name = template.name;
-      src.type = template.type;
-      src.attributes = template.attributes;
-      src.children = template.children;
-      //console.log('src',src);
+    src.name = template.name;
+    src.type = template.type;
+    src.attributes = template.attributes;
+    src.children = template.children;
   }
 }
