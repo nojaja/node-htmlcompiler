d1367dc1ff76854fdd23bd9cc5a5457ecf202183
nozawa <free.riccia@gmail.com>
2016-11-10 23:46:32 +0900

    Add IncrementalDomBuilder

--- src/index.babel.js
+++ src/index.babel.js
@@ -7,8 +7,11 @@ Copyright 2016 - 2016
 //http://yutapon.hatenablog.com/entry/2015/02/09/090000
 
 import { Builder, HtmlBuilder, CSSBuilder, Compiler} from './htmlcompiler.babel.js'
+import { IncrementalDomBuilder} from './IncrementalDomBuilder.js'
+
 
 window.Builder = Builder;
 window.HtmlBuilder = HtmlBuilder;
 window.CSSBuilder = CSSBuilder;
 window.Compiler = Compiler;
+window.IncrementalDomBuilder = IncrementalDomBuilder;
