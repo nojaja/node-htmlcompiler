b0e6fb46f1dc04d9080c3e8cac5967793aea1808
nozawa <free.riccia@gmail.com>
2016-11-10 23:46:32 +0900

    Add IncrementalDomBuilder

---
+++ src/IncrementalDomBuilder.js
@@ -0,0 +1,32 @@
+export class IncrementalDomBuilder extends window.Builder{
+    createAttribute_text(key, attribute) {
+      return(`${key}=\\'${attribute.data}\\'`);
+    };
+    createAttribute_script(key, attribute) {
+      return(`${key}='+${attribute.data}+'`);
+    };
+    createTagElement_open(src, attributes, isContainer) {
+      return(`${isContainer?'elementOpen':'elementVoid'}('${src.name}', '','${attributes}'); `);
+    }
+    createTagElement_close(src) {
+      return(`elementClose('${src.name}'); `);
+    }
+    createTextElement(src) {
+      return(`text('${src.data.replace(/\n/g,"").replace(/\'/g,"\\\'")}'); `);
+    }
+    createCommentElement(src) {
+      return(`/* ${src.data.replace(/\n/g,"").replace(/\'/g,"\\\'")} */`);
+    }  
+    createScriptElement_open(src, isContainer) {
+      if (src.name == 'if') {
+        return(`if( ${src.data}){ `);
+      }
+      if (src.name == 'each') {
+        return(`each( ${src.data}){ `);
+      }
+        return(`text( ${src.data}) `);
+    }
+    createScriptElement_close(src) {
+      return(`});`);
+    }
+}
